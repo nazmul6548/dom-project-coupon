bc1933d249ca055442bfe6b6b1e947ca1eabb10d
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-22 11:51:11 +0600

    spred

--- practice.js
+++ practice.js
@@ -1,7 +1,30 @@
-function myNum(num1=0, num2=0) {
-    const total = num1 + num2;
-    console.log(num1,num2,total);
-    return total;
+// function myNum(num1=0, num2, num3) {
+//     const total = num1 ;
+//     console.log(num1,total);
+//     return total;
     
+// }
+// const sum = myNum(3)
+
+
+const number = [1,2,3,4,]
+const students = {
+    name:"nazmul",
+    age:33
 }
-const sum = myNum()
+
+// const math = `my name is ${students.name}
+// my age is ${number[2] + students.age}}`
+// console.log(math);
+
+const myname = () => console.log('jewel');
+let a = a=> a*a
+const f =a(3)
+console.log(f);
+const hh = myname()
+// console.log(hh);
+
+
+const myArray =[3,5,6,7,8,9,10];
+const mum = Math.max(...myArray)
+console.log(mum);
